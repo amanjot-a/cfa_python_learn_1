d447af9c5d153f2f23ffe54c4c8ed3aeadc5365e
Amanjot Singh <amanjotcheema3387@gmail.com>
2026-03-02 13:16:27 +0530

    Update module2.py and add Loops.py

---
+++ Loops.py
@@ -0,0 +1,26 @@
+company_names= ["Google", "Apple", "Microsoft", "Amazon", "Facebook"]
+
+# Using a for loop to iterate through the list of company names
+
+#print(company_names[0])
+
+
+Revenues= [10000, 20000, 15000, 30000, 25000]
+
+#print(company_names[0], "Revenue for the year 2024:", Revenues[0])
+
+#lists
+
+my_list = [1, 2, 3, 4, 5]
+product= 2
+for i in my_list:
+    product = product * i
+
+# print("The product of the numbers in the list is:", product)
+
+stocks_names = ['Apple', 'Samsung', 'Alibaba', 'Novo Nordisk', 'Naspers']
+stock_prices = [142, 45, 12, 125, 30]
+
+len(stocks_names)
+for i in reversed(range(len(stocks_names))):
+    print(stocks_names[i], "stock price is:", stock_prices[i]) 
---
+++ module2.py
@@ -0,0 +1,16 @@
+Revenue_A=10000
+Revenue_B=100005
+Revenue_C=10000
+
+print(Revenue_A < Revenue_B)
+print(Revenue_A > Revenue_B)
+print(Revenue_A == Revenue_B)   
+print(Revenue_A != Revenue_B)
+
+revenue_A = 2500
+revenue_B = 2000
+
+print(revenue_A == revenue_B) 
+print(revenue_A != revenue_B) 
+print(revenue_A > revenue_B)
+print(revenue_A < revenue_B) 
